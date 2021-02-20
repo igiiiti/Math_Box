4512dd3097a6cad5f553f5acce372d81bbd6e00a
Gil Lee <nullrxn@gmail.com>
2021-02-20 10:34:59 +0900

    Add get_Sum_ver2 function

--- calculator.py
+++ calculator.py
@@ -20,5 +20,9 @@ def get_Median(a, b):
     return (a+b)/2
 
 
-def git_Sum_ver1(n):
-    return n(n+1)/2
+def git_Sum_ver2(n):
+    sum = 0
+    for i in range(1, n+1):
+        sum = sum + i
+        
+    return sum
